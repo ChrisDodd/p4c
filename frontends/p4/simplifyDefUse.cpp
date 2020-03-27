972ca71b7ad887416ba40d4165f10a1815556bff
Chris Dodd <cdodd@barefootnetworks.com>
2020-03-26 17:49:34 -0700

    Disable checkOutParams on functions due to breakge
    - add virtual4.p4 test that shows breakage

--- frontends/p4/simplifyDefUse.cpp
+++ frontends/p4/simplifyDefUse.cpp
@@ -510,11 +510,14 @@ class FindUninitialized : public Inspector {
         // We now check the out parameters using the definitions
         // produced *after* the function has completed.
         LOG3("Context after function " << currentPoint);
+#if 0
+        // FIXME -- doesn't work in the presence of abstract functions
         auto current = getCurrentDefinitions();
         checkOutParameters(func, func->type->parameters, current);
         if (saveHeaderDefs) {
             headerDefs = saveHeaderDefs;
         }
+#endif
         return false;
     }
 
