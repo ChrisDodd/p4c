2ce99ce4014114187e02b4277b0560d2f1a98453
Yongjeong Kim <yongjeongk@nvidia.com>
2023-10-25 00:59:05 -0700

    expression: Actively fail the compilation when key value is over bitWidth
    The front-end was shrinking the value when the key value is over the target
    bitwidth with warnings. However, this may lead to the duplicate const entry key
    values at the later stage. Instead of warnings, actively fails the compilation
    as this will lead to the problematic compilation output.
    
    While we do not support singed key value, add the error check for signed key
    value for unsigned key.

--- ir/expression.cpp
+++ ir/expression.cpp
@@ -94,8 +94,8 @@ void IR::Constant::handleOverflow(bool noWarning) {
         big_int min = -(one << (width - 1));
         if (value < min || value > max) {
             if (!noWarning)
-                ::P4::warning(ErrorType::WARN_OVERFLOW,
-                              "%1%: signed value does not fit in %2% bits", this, width);
+                error(ErrorType::ERR_INVALID, "%1%: signed value does not fit in %2% bits",
+                      this, width);
             LOG2("value=" << value << ", min=" << min << ", max=" << max << ", masked="
                           << (value & mask) << ", adj=" << ((value & mask) - (one << width)));
             value = value & mask;
@@ -104,12 +104,10 @@ void IR::Constant::handleOverflow(bool noWarning) {
     } else {
         if (value < 0) {
             if (!noWarning)
-                ::P4::warning(ErrorType::WARN_MISMATCH, "%1%: negative value with unsigned type",
-                              this);
+                error(ErrorType::ERR_INVALID, "%1%: negative value with unsigned type", this);
         } else if ((value & mask) != value) {
             if (!noWarning)
-                ::P4::warning(ErrorType::WARN_MISMATCH, "%1%: value does not fit in %2% bits", this,
-                              width);
+                error(ErrorType::ERR_INVALID, "%1%: value does not fit in %2% bits", this, width);
         }
 
         value = value & mask;
