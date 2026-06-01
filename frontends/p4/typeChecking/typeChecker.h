953c0fe7fe9ee722a8c591f3dd5d967d408efc1c
Chris Dodd <cdodd@nvidia.com>
2026-06-01 23:40:54 +0000

    IR::Ptr fixes for some error cases
    Signed-off-by: Chris Dodd <cdodd@nvidia.com>

--- frontends/p4/typeChecking/typeChecker.h
+++ frontends/p4/typeChecking/typeChecker.h
@@ -213,8 +213,8 @@ class TypeInferenceBase : public virtual Visitor, public ResolutionContext {
                                         const Visitor::Context *ctxt);
 
     struct Comparison {
-        const IR::Expression *left;
-        const IR::Expression *right;
+        IR::Ptr<IR::Expression> left;
+        IR::Ptr<IR::Expression> right;
     };
 
     // Helper function to handle comparisons
